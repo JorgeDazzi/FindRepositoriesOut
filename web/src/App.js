47a95667b3d08149808af684a71f2cee17b233aa
Jorge Dazzi <jorge.dazzi@gmail.com>
2020-02-15 17:18:11 +0100

    searchRepoByTerm implemented

--- web/src/App.js
+++ web/src/App.js
@@ -4,10 +4,32 @@ import RepoList from "./components/RepoList";
 import './App.css';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      repositories: [],
+      api:"http://localhost:8081/"
+    }
+  }
+
+  /**
+   * Search repositories that match what you desires
+   */
+  searchRepoByTerm = async (term)=>{
+    if(term != null){
+      const url = `${this.state.api}repo/search/${term}`;
+      let response  = await fetch(url);
+      let data = (await response).json();
+        data.then(
+          list => this.setState({repositories:list.items})
+        );
+    }
+  }
+
   render (){
     return(
       <div className="App">
-        <SearchBar/>
+        <SearchBar searchRepoByTerm={(term)=>this.searchRepoByTerm(term)}/>
 
         <div className="content">
           <RepoList />
--- web/src/components/SearchBar.js
+++ web/src/components/SearchBar.js
@@ -4,7 +4,10 @@ class SearchBar extends Component {
     render() {
         return (
             <div className="searchBar">
-                <form onSubmit={(e)=>{e.preventDefault();}}>
+                <form onSubmit={(e)=>{
+                        e.preventDefault();
+                        this.props.searchRepoByTerm(e.currentTarget.term.value);
+                    }}>
                     <input type="text" id="term" placeholder="search here Repositories in Github"/>
                     <input type="submit" value="Search" className="searchBtn"/>
                 </form>
