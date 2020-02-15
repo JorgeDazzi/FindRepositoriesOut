42c79a5bc649c90677e740aa397fe92b75b5e518
Jorge Dazzi <jorge.dazzi@gmail.com>
2020-02-15 16:28:41 +0100

    bookmarks controller getBookmarks

--- api/service/controllers/BookmarksController.js
+++ api/service/controllers/BookmarksController.js
@@ -1,3 +1,18 @@
 const fetch = require('node-fetch');
 const bm = require('../models/bookmark');
 
+const repoController = require('./RepositoriesController');
+
+exports.getBookmarks = function(res){
+    let promise = new ( async (resolve, reject) =>{
+        let book = [];
+
+        for(let i = 0; i < bm.getBookmarks().length; i++ ){
+            book.push(await repoController.getRepositoryById(bm.getBookmarks()[i]));
+        }
+
+        resolve(book);
+    })
+    .then(json => res.status(200).json(json))
+    .catch(error => res.status(400).json(error));
+}
