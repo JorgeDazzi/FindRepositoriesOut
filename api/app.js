9d8917c8a0881ce51db60272884ca8589f14a505
Jorge Dazzi <jorge.dazzi@gmail.com>
2020-02-15 14:40:38 +0100

    cors setup

--- api/app.js
+++ api/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 /* Init settings */
 const app = express();
 const port = 8081;
 
 //Body Setup
-// .....
+app.use(bodyParser.json());
+app.use(cors());
 
 
 //Endpoints Setup
